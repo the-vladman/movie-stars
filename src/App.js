d2993eb0cda63b2870b9e54fb9f57d94911cd577
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2019-04-04 10:43:11 -0600

    moviesArray hc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,41 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const movies = [
+      {
+        id: 1,
+        name: 'Batman',
+        year: 1990,
+        score: 1,
+        comments: [
+          'un comment',
+          'dos cooment',
+        ],
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Ch9thTlG8RyJ44QPtuPDze4X-Rc173z6nyG6N5Bo73TUgEc3'
+      },
+      {
+        id: 2,
+        name: 'Batman FOrever',
+        year: 1992,
+        score: 3,
+        comments: [
+          'un comment',
+          'dos cooment',
+        ],
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Ch9thTlG8RyJ44QPtuPDze4X-Rc173z6nyG6N5Bo73TUgEc3'
+      },
+      {
+        id: 3,
+        name: 'Dark Knight',
+        year: 2010,
+        score: 5,
+        comments: [
+          'un comment',
+          'dos cooment',
+        ],
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Ch9thTlG8RyJ44QPtuPDze4X-Rc173z6nyG6N5Bo73TUgEc3'
+      },
+    ]
     return (
       <div className="App">
         <header className="App-header">
