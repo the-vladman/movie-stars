58881a19cfadb9d66811cd8d64316c6eee40a15d
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2019-04-04 10:43:32 -0600

    use MovieList

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MovieList from './components/MovieList'
+
 class App extends Component {
   render() {
+    
     const movies = [
       {
         id: 1,
@@ -41,20 +44,7 @@ class App extends Component {
     ]
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MovieList movies={movies}/>
       </div>
     );
   }
